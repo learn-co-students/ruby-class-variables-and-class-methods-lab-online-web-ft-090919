176107ae95fa7e519bdf58725a334b34bd977df2
Randel Jiao <randeljiao@gmail.com>
2019-09-19 00:51:55 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song 
+  attr_accessor = :name, :artist, :genre
+  song_count = 0 
+  
+def initialize(songs)
+  @@song_count
+end
+
+def @name
+  name
+end
+
+def artist
+  @artist
+end
+
+def genre
+  @genre
+end
+
+
+
+
+
+end
